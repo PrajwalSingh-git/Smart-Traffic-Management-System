88cc6ba7dab7f771b13f61a170c1731322c0fba0
PrajwalSingh-lang <prajwal.24scse1180603@galgotiasuniversity.ac.in>
2026-07-17 20:05:07 +0530

    s

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { viteSourceLocator } from "@metagptx/vite-plugin-source-locator";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [
     viteSourceLocator({
@@ -11,7 +10,9 @@ export default defineConfig(({ mode }) => ({
     }),
     react(),
   ],
-  base: "/Smart-Traffic-Management-System",
+
+  base: "/Smart-Traffic-Management-System/",
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
