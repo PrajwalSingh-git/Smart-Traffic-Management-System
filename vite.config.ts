5a470c672ada99168b23b2143bba07ba8df0fae5
PrajwalSingh-lang <prajwal.24scse1180603@galgotiasuniversity.ac.in>
2026-07-18 11:21:36 +0530

    Configure GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,14 @@ import path from "path";
 import { viteSourceLocator } from "@metagptx/vite-plugin-source-locator";
 
 export default defineConfig(({ mode }) => ({
+  base: "/Smart-Traffic-Management-System/",
+
   plugins: [
     viteSourceLocator({
       prefix: "pra",
     }),
     react(),
   ],
-
-
 
   resolve: {
     alias: {
